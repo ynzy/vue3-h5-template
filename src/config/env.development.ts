f72154eb6c40bb9eb986d562cedf9a77bd7b2bb3
勇 张 <758922096@qq.com>
2021-02-21 10:38:28 +0800

    feat(model:all): 添加微信证书

--- src/config/env.development.ts
+++ src/config/env.development.ts
@@ -5,7 +5,7 @@ module.exports = {
   title: '开发',
   baseUrl: 'http://localhost:9018', // 项目地址
   baseApi: 'https://test.xxx.com/api', // 本地api请求地址,注意：如果你使用了代理，请设置成'/'
-  APPID: 'xxx',
+  APPID: 'wxcf328ba1b9fc2be2',
   APPSECRET: 'xxx',
   $cdn: 'https://imgs.solui.cn'
 }
