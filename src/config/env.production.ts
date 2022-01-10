2aa99317a6a3275887316be87c2bb522ace3f21d
ynzy <758922096@qq.com>
2022-01-10 22:18:55 +0800

    feat: 更换appid

--- src/config/env.production.ts
+++ src/config/env.production.ts
@@ -5,7 +5,7 @@ module.exports = {
   title: '生产',
   baseUrl: 'https://www.xxx.com/', // 正式项目地址
   baseApi: 'https://www.xxx.com/api', // 正式api请求地址
-  APPID: 'wx9790364d20b47d95',
+  APPID: 'wxcf328ba1b9fc2be2',
   APPSECRET: 'xxx',
   $cdn: 'https://imgs.solui.cn'
 }
